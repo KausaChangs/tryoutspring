41f0c37e323e0090afbe2161e9b9c99e8e5954c3
KausaChangs <changayakausa@gmail.com>
2023-02-22 08:48:39 +0200

    my first commit

---
+++ tryoutspring/src/main/java/com/kausa/rodrick/tryoutspring/TryoutspringApplication.java
@@ -0,0 +1,13 @@
+package com.kausa.rodrick.tryoutspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TryoutspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TryoutspringApplication.class, args);
+	}
+
+}
---
+++ tryoutspring/src/test/java/com/kausa/rodrick/tryoutspring/TryoutspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kausa.rodrick.tryoutspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TryoutspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
